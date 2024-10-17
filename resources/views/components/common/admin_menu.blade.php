8f6e997db829ea56041657d7034ffc3a0fd4a560
Your Name <you@example.com>
2024-10-16 22:11:36 -0300

    Menu de los roles

---
+++ resources/views/components/common/admin_menu.blade.php
@@ -0,0 +1,24 @@
+<div>
+    <nav class="w-full h-auto mt-32 duration-300">
+        <ul class="w-full h-full">
+            <li class="w-full pl-4 cursor-pointer p-3 hover:bg-red-400 text-xl duration-300" >
+                <a><i class="w-1/6 fas fa-user"></i>Pacientes</a>
+            </li>
+            <li class="w-full pl-4 cursor-pointer p-3 hover:bg-red-400 text-xl duration-300" >
+                <a><i class="w-1/6 fas fa-user-md"></i>Médicos</a>
+            </li>
+            <li class="w-full pl-4 cursor-pointer p-3 hover:bg-red-400 text-xl duration-300" >
+                <a><i class="w-1/6 fas fa-calendar-times"></i>Calendario General</a>
+            </li>
+            <li class="w-full pl-4 cursor-pointer p-3 hover:bg-red-400 text-xl duration-300" >
+                <a><i class="w-1/6 fas fa-chart-pie"></i>Informes y Estadísticas</a>
+            </li>
+            <li class="w-full pl-4 cursor-pointer p-3 hover:bg-red-400 text-xl duration-300" >
+                <a><i class="w-1/6 fas fa-id-card"></i>Permisos de Usuarios</a>
+            </li>
+            <li class="w-full pl-4 cursor-pointer p-3 hover:bg-red-400 text-xl duration-300" >
+                <a><i class="w-1/6 fas fa-pencil-alt"></i>Inventario Médico</a>
+            </li>
+        </ul>
+    </nav>        
+</div>
---
+++ resources/views/components/common/doctor_menu.blade.php
@@ -0,0 +1,21 @@
+<div>
+    <nav class="w-full h-auto mt-32 duration-300">
+        <ul class="w-full h-full">
+            <li class="w-full pl-4 cursor-pointer p-3 hover:bg-red-400 text-xl duration-300" >
+                <a><i class="w-1/6 fas fa-calendar-day"></i>Consultas del Día</a>
+            </li>
+            <li class="w-full pl-4 cursor-pointer p-3 hover:bg-red-400 text-xl duration-300" >
+                <a><i class="w-1/6 fas fa-calendar-alt"></i>Historial de Consultas</a>
+            </li>
+            <li class="w-full pl-4 cursor-pointer p-3 hover:bg-red-400 text-xl duration-300" >
+                <a><i class="w-1/6 fas fa-sticky-note"></i>Recetas Emitidas</a>
+            </li>
+            <li class="w-full pl-4 cursor-pointer p-3 hover:bg-red-400 text-xl duration-300" >
+                <a><i class="w-1/6 fas fa-calendar-times"></i>Calendario</a>
+            </li>
+            <li class="w-full pl-4 cursor-pointer p-3 hover:bg-red-400 text-xl duration-300" >
+                <a><i class="w-1/6 fas fa-align-center"></i>Informes Médicos</a>
+            </li>
+        </ul>
+    </nav>        
+</div>
