aa46a4a0d868545a4b8df66596f82ce45ea97a8d
Your Name <you@example.com>
2025-06-19 13:43:00 -0300

    Formulario crear orden de compra

--- resources/views/livewire/pages/admin/purchase-orders.blade.php
+++ resources/views/livewire/pages/admin/purchase-orders.blade.php
@@ -39,53 +39,50 @@
             {{-- Modal para ver los reportes --}}
             <div x-show="showPurchaseOrders" class="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
                 <div class="w-3/5 bg-white p-6 rounded-lg shadow-lg max-h-[80vh] overflow-y-auto">
-                    <h2 class="text-2xl text-center">Generar orden de compra</h2>
+                    <h2 class="text-2xl text-center">Generar ordenes de compra</h2>
 
 
 
 
 
 
-                    <div>
-                        <table border="1" cellpadding="10" class="mt-5">
-                            <thead>
-                                <tr>
-                                    <th class="bg-red-500 text-white">Proveedor</th>
-                                    <th class="bg-red-500 text-white">Producto</th>
-                                    <th class="bg-red-500 text-white">Stock de Reposición</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @forelse($groupedBySupplier as $supplier)
-                                @php
-                                $products = $supplier->products;
-                                $rowspan = $products->count();
-                                @endphp
+                    <div class="mt-5">
+                        @forelse($groupedBySupplier as $supplier)
+                        <div class="mb-8">
+                            <div class="w-full flex items-center justify-between">
+                                <h2 class="text-lg font-bold text-gray-700 mb-3">Proveedor: {{ $supplier->name }}</h2>
+                                <button class="w-auto h-12 px-3 mb-6 rounded-md duration-300 bg-green-400 hover:bg-green-500">
+                                    Generar orden
+                                    <i class="fas fa-file-invoice-dollar"></i>
+                                </button>
+                            </div>
 
-                                @if($rowspan > 0)
-                                @foreach($products as $index => $product)
-                                <tr>
-                                    @if($index === 0)
-                                    <td rowspan="{{ $rowspan }}" class="mt-12">{{ $supplier->name }}</td>
-                                    @endif
-                                    <td class="text-center">{{ $product->name }}</td>
-                                    <td class="text-center">{{ $product->stock_reposition }}</td>
-                                </tr>
-                                @endforeach
-                                @endif
-                                @empty
-                                <tr>
-                                    <td colspan="4">No hay productos con stock bajo.</td>
-                                </tr>
-                                @endforelse
-                            </tbody>
-                        </table>
-
-
-
-
-
+                            <table border="1" cellpadding="10" class="w-full">
+                                <thead>
+                                    <tr>
+                                        <th class="bg-red-500 text-sm text-white text-center">Producto</th>
+                                        <th class="bg-red-500 text-sm text-white text-center">Precio/Unidad</th>
+                                        <th class="bg-red-500 text-sm text-white text-center">Cantidad</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($supplier->products as $product)
+                                    <tr>
+                                        <td class="text-center text-sm w-1/3">{{ $product->name }}</td>
+                                        <td class="text-center text-sm w-1/3">{{ $product->price }}</td>
+                                        <td>
+                                            <input class="w-24 block m-auto placeholder:text-gray-500 rounded-md border-2 border-red-400" type="number" min="{{ $product->stock_reposition }}" wire:model="quantity" placeholder="{{ $product->stock_reposition }}">
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                        @empty
+                        <p>No hay productos con stock bajo.</p>
+                        @endforelse
                     </div>
+
 
                     <button @click="showPurchaseOrders = false" class="block m-auto mt-5 px-5 bg-gray-600 hover:bg-gray-700 text-white p-2">
                         Cerrar
